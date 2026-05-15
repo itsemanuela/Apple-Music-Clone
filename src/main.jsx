ce48d11fdfef71b7faa30503669a7b6be048dd58
itsemanuela <notemanu152@gmail.com>
2026-05-15 09:31:29 +0200

    Piano strutturale e organizzazione cartelle interne
    Components, Redux, Store, Reducers, Actions

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
